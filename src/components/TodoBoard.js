9931a3fd49575ab5cd38f418ecad2bb17c5bd0bd
이수하 <swuhalee@gmail.com>
2026-02-05 17:06:15 +0900

    fix: todos undefined 에러 방지

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -5,7 +5,7 @@ const TodoBoard = ({ todos, getTasks }) => {
 
     <div>
       <h2>Todo List</h2>
-      {todos.length > 0 ? todos.map(todo => <TodoItem key={todo.id} todo={todo} getTasks={getTasks} />) : <h2>There is no Item to show</h2>}
+      {todos?.length > 0 ? todos.map(todo => <TodoItem key={todo.id} todo={todo} getTasks={getTasks} />) : <h2>There is no Item to show</h2>}
     </div>
   );
 };
